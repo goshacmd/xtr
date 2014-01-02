392c623ca84ff743f9bf6dc0b9ac0ff97dd134a5
Gosha Arinich <me@goshakkk.name>
2014-01-02 16:19:02 +0300

    simple delegation from account to balance

--- lib/xtr/account.rb
+++ lib/xtr/account.rb
@@ -37,51 +37,13 @@ module Xtr
     end
     alias [] balance
 
-    # Public: Credit funds to the `instrument` balance.
+    # Delegate balance-related methods to balance for appropriate instrument.
     #
-    # instrument - The Symbol instrument name.
-    # amount     - The amount to credit.
-    def credit(instrument, amount)
-      balance(instrument).credit(amount)
-    end
-
-    # Public: Debit funds from the `instrument` balance.
+    # Examples
     #
-    # instrument - The Symbol instrument name.
-    # amount     - The amount to debit.
-    def debit(instrument, amount)
-      balance(instrument).debit(amount)
-    end
-
-    # Public: Reserve funds from the `instrument` balance.
-    #
-    # instrument - The Symbol instrument name.
-    # amount     - The amount to reserve.
-    #
-    # Returns a String reservation identifier.
-    def reserve(instrument, amount)
-      balance(instrument).reserve(amount)
-    end
-
-    # Public: Release funds from the `instrument` reserve.
-    #
-    # instrument - The Symbol instrument name
-    # reserve_id - The String reservation identifier.
-    # amount     - The optional amount to release. If not
-    #              passed, all remaining funds will be released.
-    def release(instrument, reserve_id, amount)
-      balance(instrument).release(reserve_id, amount)
-    end
-
-    # Public: Debit funds from the `instrument` reserved balance.
-    #
-    # instrument - The Symbol instrument name.
-    # reserve_id - The String reservation identifier.
-    # amount     - The optional amount to release. If not
-    #              passed, all remaining funds will be released.
-    def debit_reserved(instrument, reserve_id, amount = nil)
-      balance(instrument).debit_reserved(reserve_id, amount)
-    end
+    #   account.credit(USD, 100.00)
+    delegate :credit, :debit, :reserve, :release, :debit_reserved,
+      to: 'balance(args.shift)'
 
     def to_s
       balances = @balances.values.sort_by { |b| b.instrument.name }
